35e8e23c54cf1a482202fa6899d9844423cee814
joshu <kemboijoshua04@gmail.com>
2019-02-10 15:30:20 +0300

    [starts #163847813] creates a political office

---
+++ app/API/v1/models/user_record_models.py
@@ -0,0 +1,62 @@
+all_users = [{
+
+        "first_name" : "first_name",
+        "second_name" :"second_name",
+        "other_name" : "other_name",
+        "email": "taken_email@gmail.com",
+        "phone_number": "0720000000",
+        "passport_url": "url",
+        "password": "Joshu455s*",
+        "repeat_password": "Joshu455s*"
+    }]
+
+admins = [{
+        "first_name" : "Josh",
+        "second_name" :"kemboi",
+        "other_name" : "rop",
+        "email": "joshu@gmail.com",
+        "phone_number": "0702983637",
+        "passport_url": "url",
+        "password": "Joshu455s*",
+        "repeat_password": "Joshu455s*"
+    }]
+
+
+class UserRecord:
+    def __init__(self):
+        self.all_users = all_users
+        self.admins = admins
+
+    def create_user(self,first_name ,second_name,other_name, email , phone_number,passport_url , password , repeat_password):
+        def is_admin():
+            for admin in self.admins:
+                if email in admin.values():
+                    return True
+                if phone_number in admin.values():
+                    return True
+                else:
+                    return False
+        new_user = {
+            "id":len(self.all_users)-1,
+            "first_name" : first_name,
+            "second_name" : second_name,
+            "other_name" : other_name,
+            "email": email,
+            "phone_number": phone_number,
+            "is_admin":is_admin(),
+            "passport_url":passport_url,
+            "password": password,
+            "repeat_password": repeat_password,
+        }
+        if new_user:
+            self.all_users.append(new_user)
+        return new_user
+
+    def get_all_users(self):
+        return self.all_users
+
+    def get_single_user(self, username):
+        usr = [usr for usr in all_users if usr[username] == username]
+        if usr:
+            return usr
+        return None
---
+++ app/API/v1/views/office_record_views.py
@@ -0,0 +1,34 @@
+import re
+from ..models.office_record_models import OfficeRecord , all_offices
+from flask_restful import Resource , reqparse , Api,abort
+from app import app, api
+
+office_parser = reqparse.RequestParser()
+office_parser.add_argument('office_type', type=str, help='Enter office type', required=True)
+office_parser.add_argument('office_name', type=str, help='Enter office type', required=True)
+
+class Office(Resource):
+    def get(self):
+        offices = OfficeRecord()
+        return {"data":offices.get_all_offices()},200
+
+    def post(self):
+        office_args = office_parser.parse_args()
+        office_type =office_args['office_type']
+        office_name =office_args['office_name']
+        if not office_type or not office_name:
+            return {"message":"All the fields must be filled"}
+        office = OfficeRecord()
+        office = office.create_office(office_type , office_name)
+        return office, 201
+
+
+class SingleOffice(Resource):
+    def get(self,office_id):
+        single_office = OfficeRecord()
+        return single_office.get_single_office(office_id)
+
+
+
+api.add_resource(SingleOffice, '/office/<int:office_id>')
+api.add_resource(Office, '/office')
