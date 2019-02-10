ac54bc3ae8d664915282ea37d8f71ba5ee86b535
joshu <kemboijoshua04@gmail.com>
2019-02-10 17:27:50 +0300

    [starts #163848388]deletes a political office

--- app/tests/v1/test_user_record_views.py
+++ app/tests/v1/test_user_record_views.py
@@ -29,7 +29,7 @@ class Test_test(unittest.TestCase):
         self.assertEqual(response.status_code,400)
 
     def test_user_registration_different_passwords(self):
-        response = self.signup(first_name='joshua',second_name = "Kimutai",other_name = "chelimo",
+        response = self.signup(first_name='joshua',second_name = "kemboi",other_name = "rop",
         email='kim@gmail.com',phone_number='0799087221',passport_url = "passport",password='HHeezziiee1357&%',
         repeat_password='HHeezziiee13%')
         self.assertEqual(response.status_code,400)
@@ -41,31 +41,31 @@ class Test_test(unittest.TestCase):
         self.assertEqual(response.status_code,400)
 
     def test_signup_taken_email(self):
-        response = self.signup(first_name='Hezron',second_name = "Kimutai",other_name = "chelimo",
+        response = self.signup(first_name='Hezron',second_name = "kemboi",other_name = "rop",
         email='taken_email@gmail.com',phone_number='+254799087221',passport_url = "passport",password='HHeezziiee1357&%#',
         repeat_password='HHeezziiee1357&%#')
         self.assertEqual(response.status_code,400)
 
     def test_signup_taken_phone_number(self):
-        response = self.signup(first_name='Hezron',second_name = "Kimutai",other_name = "chelimo",
+        response = self.signup(first_name='joshua',second_name = "kemboi",other_name = "rop",
         email='email@gmail.com',phone_number='+254700000000',passport_url = "passport",password='HHeezziiee1357&%#',
         repeat_password='HHeezziiee1357&%#')
         self.assertEqual(response.status_code,400)
 
     def test_signup_invalid_email(self):
-        response = self.signup(first_name='Hezron',second_name = "Kimutai",other_name = "chelimo",
+        response = self.signup(first_name='joshua',second_name = "kemboi",other_name = "rop",
         email='emailgmail.com',phone_number='+254700000000',passport_url = "passport",password='HHeezziiee1357&%#',
         repeat_password='HHeezziiee1357&%#')
         self.assertEqual(response.status_code,400)
 
     def test_signup_invalid_phone(self):
-        response = self.signup(first_name='Hezron',second_name = "Kimutai",other_name = "chelimo",
+        response = self.signup(first_name='joshua',second_name = "kemboi",other_name = "rop",
         email='emailgmail.com',phone_number='0700000000',passport_url = "passport",password='HHeezziiee1357&%#',
         repeat_password='HHeezziiee1357&%#')
         self.assertEqual(response.status_code,400)
 
     def test_signup_missing_fields(self):
-        response = self.signup_missing_fields(first_name='hezron',second_name = "Kimutai",other_name = "chelimo",
+        response = self.signup_missing_fields(first_name='joshua',second_name = "kemboi",other_name = "rop",
         email='emailgmail.com',password='HHeezziiee1357&%#',
         repeat_password='HHeezziiee1357&%#')
         self.assertEqual(response.status_code,400)
